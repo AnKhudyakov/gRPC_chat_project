b40b1e1560a5b06fd68f304fac9f6f33b753cfb0
Ivan <i.generalov.93@mail.com>
2022-12-11 13:44:47 +0500

    feat: add routing to login and registration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 import {
@@ -15,12 +16,13 @@ import { Login } from "./features/Login/Login";
 //export const client = new AuthServiceClient("http://localhost:8080");
 
 function App() {
-  
   return (
     <div className="App">
       <main>
-        <Auth />
-        <Login />
+        <Routes>
+          <Route path="/" element={<Login />}></Route>
+          <Route path="registration" element={<Auth />}></Route>
+        </Routes>
       </main>
     </div>
   );
