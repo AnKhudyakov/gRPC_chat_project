075203024b4730f432422b7d503ce32c802b0187
Daniil Simanovich <s1mahochukvas@vk.com>
2022-12-13 16:38:50 +0300

    styles modified && added fonts and root>vart

--- frontend/src/components/UserBlock/UserBlock.jsx
+++ frontend/src/components/UserBlock/UserBlock.jsx
@@ -6,12 +6,12 @@ const UserBlock = ({ data, profile = false }) => {
     const Content = () => {
         return !profile ? (
             <div className={styles.person}>
-                <div className={styles.person__info}>
-                    <span className={styles.name}>{username} </span>
-                    <span className={styles.id}>ID: {id}</span>
+                <div className={styles.img_container}>
+                    <img src="/blank-profile-picture.png" alt="profile picture" />
                 </div>
-                <div className={styles.person__lastMess}>
-                    {lastMess}
+                <div className={styles.person__info}>
+                    <span className={styles.name}>{username}</span>
+                    <span className={styles.id}>#{id}</span>
                 </div>
             </div>
         ) : (
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import {BrowserRouter} from 'react-router-dom'
 import store from "./app/store";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "./index.css";
+import "./styles/globals.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
